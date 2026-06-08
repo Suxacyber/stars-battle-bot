c230853f4241ecde4caa3cb5b3a4552120592834
Suxacyber <mamadaliyevsuhrob90@gmail.com>
2026-06-08 09:56:16 +0500

    Create store/index.js

---
+++ store/index.js
@@ -0,0 +1,10 @@
+// Store index exports
+const db = require('./db');
+const Battle = require('./models/Battle');
+const Payment = require('./models/Payment');
+
+module.exports = {
+  db,
+  Battle,
+  Payment
+};
